836b4effc2a2615ace5a10a4330c999c76223c46
Shanen Cross <shanencross@yahoo.com>
2021-06-23 20:31:10 -0700

    Add sample thread data
    Co-authored-by: Faisal Rana <inquisitive@gmail.com>

--- ConsoleWarsForum/Models/ConsoleWarsForumContext.cs
+++ ConsoleWarsForum/Models/ConsoleWarsForumContext.cs
@@ -6,8 +6,13 @@ namespace ConsoleWarsForum.Models {
     public virtual DbSet<Thread> Threads { get; set; }
     public ConsoleWarsForumContext(DbContextOptions options) : base(options) { }
 
-    // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
-    //   optionsBuilder.UseLazyLoadingProxies();
-    // }
+    protected override void OnModelCreating(ModelBuilder builder) {
+      builder.Entity<Thread>()
+        .HasData(
+          new Thread { ThreadId = 1, Title = "Sega Genesis vs Super Nintendo Entertainment System", Body = "Everyone knows that Sega does with NintenDON'T", DateAndTimeStamp=System.DateTime.Now },
+          new Thread { ThreadId = 2, Title = "Nintendo 64 vs Sony PlayStation vs Sega Saturn", Body = "PlayStation was the clear winner", DateAndTimeStamp=System.DateTime.Now},
+          new Thread { ThreadId = 3, Title = "PlayStation 2 vs Nintendo GameCube vs Microsoft Xbox vs Sega Dreamcast", Body = "The Xbox was the best because it had Halo", DateAndTimeStamp=System.DateTime.Now}
+        );
+    }
   }
 }
